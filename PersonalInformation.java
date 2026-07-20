26ae492ed6a69bd75970d8963ddae903d4d1ccbc
Noviehm <vieealonsabe@gmail.com>
2026-07-20 13:56:04 +0800

    Add files via upload

---
+++ PersonalInformation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class PersonalInformation {
+    public static void main(String[] args) {
+    
+        String petType = "Dog Person";
+        String name = "Noviehm Grace Alonsabe (Bembem)";
+        String birthday = "11/11/2006";
+        String address = "Anino St., Alimodian";
+        String favoriteSong = "Gitara / Parokya ni Edgar";
+        String motivation = "My motivation is to give something back to my loved ones someday.";
+        String support = "Shorter, consise and focused PowerPoint presentations that leave room for open Q&A.";
+
+        System.out.println("=== PERSONAL INFORMATION ===");
+        System.out.println("Pet Type: " + petType);
+        System.out.println("Name: " + name);
+        System.out.println("Birthday: " + birthday);
+        System.out.println("Address: " + address);
+        System.out.println("Favorite Song: " + favoriteSong);
+        System.out.println("Motivation: " + motivation);
+        System.out.println("Support: " + support);
+    
+}
+}
